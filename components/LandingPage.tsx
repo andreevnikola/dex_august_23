9060b64671f20672b80c45a79231dd163c8505b8
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-12 19:36:18 +0300

    fixed landing

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { SignUpButton } from "@clerk/nextjs";
 import Footer from "./Footer";
 
 export default function LandingPage() {
@@ -12,14 +13,19 @@ export default function LandingPage() {
           />
           <div>
             <h1 className="text-5xl font-bold text-neutral-focus">
-              Box Office News!
+              DEX - експресни доставки
             </h1>
             <p className="py-6 text-neutral">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              <strong>DEX</strong> предлага най-бързите градски доставки.
+              Изпълнението на доставката започва до <strong>2 минути</strong>{" "}
+              след заявяването и, а колата ни е на адрес на изпращача в рамките
+              на <strong>5 мин.</strong> Това е възможно, защото{" "}
+              <strong>DEX</strong> се диверява на таксита за изпълението на
+              пратката, а само в Пловдив колите ни са над <strong>5000</strong>
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <SignUpButton mode="modal">
+              <button className="btn btn-primary">Да започваме</button>
+            </SignUpButton>
           </div>
         </div>
       </main>
