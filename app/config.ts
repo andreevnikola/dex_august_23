10d23dba40207fecf87ebbb18b9d5b45ae647c68
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-12 16:23:22 +0300

    added 404 page

---
+++ app/config.ts
@@ -0,0 +1,1 @@
+export const supportMail = "support.dexbg@gmail.com";
---
+++ app/not-found.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { supportMail } from "./config";
+
+export default function Custom404() {
+  const router = useRouter();
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src="/images/notFound.png"
+          className="max-w-sm rounded-lg shadow-2xl w-full"
+        />
+        <div>
+          <h1 className="text-5xl font-bold text-neutral-focus">
+            Страницата не е намерена!
+          </h1>
+          <p className="py-6 text-neutral">
+            Не съществува такава страница. Проверетае URL адреса, въведен от
+            Вас. Ако сте сигорни че това е правилния URL адрес е възможно
+            страницата да е била изтрита или преместена. За помощ се свържете с
+            нас на <strong>{supportMail}</strong>
+          </p>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            Върни се
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
