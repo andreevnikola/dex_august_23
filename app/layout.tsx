a08df777e51528fde0a652da682d7a8c67a13b52
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-16 12:48:54 +0300

    added viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dex - Експресни доставки",
   description: "Express deliveryis using taxies!",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
