ac842b2c2303bf9abea81506087f365253745bb1
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-26 17:45:49 +0300

    clerk user related event handled

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -18,7 +18,6 @@ const supabase = createClient(
 );
 
 const handler = async (req: Request) => {
-  console.log("doinf work my brothers");
   const payload = await req.json();
   const headersList = headers();
   const heads = {
@@ -32,17 +31,59 @@ const handler = async (req: Request) => {
   let evt: WebhookEvent | undefined;
   try {
     // Verify the webhook payload and headers
-    evt = wh.verify(payload, heads as any) as WebhookEvent;
+    evt = wh.verify(JSON.stringify(payload), heads as any) as WebhookEvent;
   } catch (err) {
     console.error(err);
     return new NextResponse(null, { status: 400 });
   }
-  const { id } = evt.data;
 
   const eventType = evt.type;
   switch (eventType) {
+    case "user.created":
+      const { error: creationError } = await supabase.from("users").insert({
+        id: evt.data.id,
+        first_name: evt.data.first_name,
+        last_name: evt.data.last_name,
+        email: evt.data.email_addresses.find(
+          (mail) => mail.id === (evt!.data as any).primary_email_address_id
+        ),
+        phone: evt.data.phone_numbers.find(
+          (phone) => phone.id === (evt!.data as any).primary_phone_number_id
+        ),
+      });
+      if (creationError) {
+        console.error(creationError);
+        return new NextResponse(null, { status: 500 });
+      }
+      break;
     case "user.updated":
-      console.log(`User ${id} was ${eventType}`);
+      const { error: updationError } = await supabase
+        .from("users")
+        .update({
+          first_name: evt.data.first_name,
+          last_name: evt.data.last_name,
+          email: evt.data.email_addresses.find(
+            (mail) => mail.id === (evt!.data as any).primary_email_address_id
+          ),
+          phone: evt.data.phone_numbers.find(
+            (phone) => phone.id === (evt!.data as any).primary_phone_number_id
+          ),
+        })
+        .eq("id", evt.data.id);
+      if (updationError) {
+        console.error(updationError);
+        return new NextResponse(null, { status: 500 });
+      }
+      break;
+    case "user.deleted":
+      const { error: deletionError } = await supabase
+        .from("users")
+        .delete()
+        .eq("id", evt.data.id);
+      if (deletionError) {
+        console.error(deletionError);
+        return new NextResponse(null, { status: 500 });
+      }
       break;
   }
   return new NextResponse(null, { status: 201 });
