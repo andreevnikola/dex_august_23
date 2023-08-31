8f95ba8e614bc46b2cd529ed4f0be887e5fadfc7
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-31 11:43:28 +0300

    added authenticated procedure

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -124,7 +124,6 @@ export default function Actions() {
           </div>
         </div>
       </div>
-      <Footer />
     </>
   );
 }
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,5 +1,43 @@
-import { initTRPC } from "@trpc/server";
+import { db } from "@/app/_db/prisma";
+import { TRPCError, inferAsyncReturnType, initTRPC } from "@trpc/server";
+import * as trpc from "@trpc/server/adapters/express";
+import jwt from "jsonwebtoken";
 
-const t = initTRPC.create();
+export const createContext = async ({
+  req,
+  res,
+}: trpc.CreateExpressContextOptions) => {
+  const clientToken = req.headers.authorization?.split(" ")[0];
+
+  const decoded = clientToken
+    ? jwt.verify(clientToken, process.env.PEM_PUBLIC_KEY!)
+    : null;
+
+  return {
+    user: decoded
+      ? await db.user.findUnique({
+          where: {
+            id: decoded?.sub as string,
+          },
+        })
+      : null,
+  };
+};
+type Context = inferAsyncReturnType<typeof createContext>;
+export const t = initTRPC.context<Context>().create();
+
+const isAuthed = t.middleware((opts) => {
+  const { ctx } = opts;
+  if (!ctx.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return opts.next({
+    ctx: {
+      user: ctx.user,
+    },
+  });
+});
+
 export const router = t.router;
-export const publicProcedure = t.procedure;
+export const puclicProcedure = t.procedure;
+export const authedProcedure = t.procedure.use(isAuthed);
