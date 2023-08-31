df72e499e368af9ee0ade501071be8275eb991cd
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-08-31 10:41:56 +0300

    user model connected to clerk usaers

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,6 +2,7 @@ import type { WebhookEvent } from "@clerk/nextjs/server";
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { db } from "@/app/_db/prisma";
 
 const webhookSecret: string = process.env.WEBHOOK_SECRET!;
 
@@ -26,55 +27,51 @@ const handler = async (req: Request) => {
   }
 
   const eventType = evt.type;
-  switch (eventType) {
-    case "user.created":
-      const { error: creationError } = await supabase.from("users").insert({
-        id: evt.data.id,
-        first_name: evt.data.first_name,
-        last_name: evt.data.last_name,
-        email: evt.data.email_addresses.find(
-          (mail) => mail.id === (evt!.data as any).primary_email_address_id
-        ),
-        phone: evt.data.phone_numbers.find(
-          (phone) => phone.id === (evt!.data as any).primary_phone_number_id
-        ),
-      });
-      if (creationError) {
-        console.error(creationError);
-        return new NextResponse(null, { status: 500 });
-      }
-      break;
-    case "user.updated":
-      const { error: updationError } = await supabase
-        .from("users")
-        .update({
-          first_name: evt.data.first_name,
-          last_name: evt.data.last_name,
-          email: evt.data.email_addresses.find(
-            (mail) => mail.id === (evt!.data as any).primary_email_address_id
-          ),
-          phone: evt.data.phone_numbers.find(
-            (phone) => phone.id === (evt!.data as any).primary_phone_number_id
-          ),
-          profile_picture: evt.data.image_url,
-        })
-        .eq("id", evt.data.id);
-      if (updationError) {
-        console.error(updationError);
-        return new NextResponse(null, { status: 500 });
-      }
-      break;
-    case "user.deleted":
-      const { error: deletionError } = await supabase
-        .from("users")
-        .delete()
-        .eq("id", evt.data.id);
-      if (deletionError) {
-        console.error(deletionError);
-        return new NextResponse(null, { status: 500 });
-      }
-      break;
+  try {
+    switch (eventType) {
+      case "user.created":
+        await db.user.create({
+          data: {
+            id: evt.data.id,
+            first_name: evt.data.first_name,
+            last_name: evt.data.last_name,
+            email: evt.data.email_addresses.find(
+              (mail) => mail.id === (evt!.data as any).primary_email_address_id
+            )!.email_address,
+            phone: evt.data.phone_numbers.find(
+              (phone) => phone.id === (evt!.data as any).primary_phone_number_id
+            )!.phone_number,
+            image_url: evt.data.image_url,
+          },
+        });
+        break;
+      case "user.updated":
+        await db.user.update({
+          where: {
+            id: evt.data.id,
+          },
+          data: {
+            first_name: evt.data.first_name,
+            last_name: evt.data.last_name,
+            email: evt.data.email_addresses.find(
+              (mail) => mail.id === (evt!.data as any).primary_email_address_id
+            )!.email_address,
+            phone: evt.data.phone_numbers.find(
+              (phone) => phone.id === (evt!.data as any).primary_phone_number_id
+            )!.phone_number,
+            image_url: evt.data.image_url,
+          },
+        });
+        break;
+      case "user.deleted":
+        await db.user.delete({ where: { id: evt.data.id } });
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(null, { status: 500 });
   }
+
   return new NextResponse(null, { status: 201 });
 };
 
